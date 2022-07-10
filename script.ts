92728b803922e6de7c112b309b2a0a69c7a4169b
yoshi_desu <xingdiansongtian@gmail.com>
2022-07-10 21:58:33 +0900

    prisumaのdemo

--- script.ts
+++ script.ts
@@ -1,16 +1,18 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 // A `main` function so that you can use async/await
 async function main() {
   // ... you will write your Prisma Client queries here
+  const result = await prisma.user.findMany();
+  console.log(result);
 }
 
 main()
-  .catch(e => {
-    throw e
+  .catch((e) => {
+    throw e;
   })
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });
